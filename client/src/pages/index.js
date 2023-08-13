4254bb8dcf644105cea08d999c839d6d5cf623fe
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-08-13 12:30:03 +0700

    test

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,13 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Home from './HomePage/Home'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function MainPage() {
   return (
     <>
      Hello world
+     <Home/>
     </>
   )
 }
