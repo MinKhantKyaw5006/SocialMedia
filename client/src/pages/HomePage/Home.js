83d52a1184316cdca8375e567fd7f6527772a493
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-09-12 21:42:23 +0700

    update

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import style from './styles.module.css'
+
 
 const Home = () => {
-  return (
-    <div className={style['parent-container']}> {/* Add a parent container */}
-    <div className={style['custom-rectangle']}>
-      {/* Content */}
-    </div>
-  </div>
-  );
+    return (
+        <div >
+            
+         
+        </div>
+    );
 }
 
 export default Home;
